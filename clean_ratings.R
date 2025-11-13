3bf8c8187d146e7cd58577f4602e4864225c6c0c
Lauren Schilling <135267204+laurenpschilling@users.noreply.github.com>
2025-11-12 22:25:43 -0500

    Add clean_ratings

---
+++ clean_ratings.R
@@ -0,0 +1,22 @@
+# load raw verb embodiment ratings
+verb_rate_wide <- read.csv('https://raw.githubusercontent.com/laurenpschilling/verb_embodiment/refs/heads/main/verb_embod_rate_raw.csv')
+
+# clean colnames
+colnames(verb_rate_wide) <- gsub("^To.", "", colnames(verb_rate_wide))
+
+# calculate verb rating mean & sd
+verb_embod_wide <- verb_rate_wide %>% 
+  summarise(across(everything(), list(mean=~mean(. , na.rm=TRUE), sd=~sd(., na.rm=TRUE))))
+
+# create a long dataframe with mean & sd of each word
+verb_embod_rate <- verb_embod_wide %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = "var_stat",
+    values_to = "value"
+  ) %>%
+  separate(var_stat, into = c("variable", "stat"), sep = "_") %>%
+  pivot_wider(names_from = stat, values_from = value) %>% 
+  rename(word = variable,
+         embod_m = mean,
+         embod_sd = sd)
